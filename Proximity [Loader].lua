414c9dc70a92afb3576040cb7ee5bb56148f81c1
SkyCoded <EdgarsJanis@outlook.com>
2016-08-02 22:38:16 +0300

    Update Proximity [Loader].lua

--- Proximity [Loader].lua
+++ Proximity [Loader].lua
@@ -1,1 +1,9 @@
 --//Warning this is not Proximity Source / This is Proximity Loader
+
+script.Name = "Proximity"
+script.Parent = nil
+wait()
+local script = require(469240284)
+warn("Proximity Loaded!")
+warn("Say cmds> to see list of Proximity Commands!")
+
