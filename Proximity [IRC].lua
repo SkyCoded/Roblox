7b6ba95f7cd30120cb89e30b781bd3fbcbe73626
SkyCoded <EdgarsJanis@outlook.com>
2016-08-05 14:06:50 +0300

    Update Proximity [IRC].lua

--- Proximity [IRC].lua
+++ Proximity [IRC].lua
@@ -266,202 +266,31 @@ function GetPlayer(msg,Reason)
 	end
 	return FoundPlayers
 end
-Scale = function(p, size)
-	local pchar = p.Character
-	if pchar then
-		local function scale(chr, scl)
-       
-			for _, v in pairs(pchar:GetChildren()) do
-				if v:IsA("Hat") then
-					v:Clone()
-					v.Parent = game.Lighting
-				end
-			end
-               
-			local Head = chr['Head']
-			local Torso = chr['Torso']
-			local LA = chr['Left Arm']
-			local RA = chr['Right Arm']
-			local LL = chr['Left Leg']
-			local RL = chr['Right Leg']
-			local HRP = chr['HumanoidRootPart']
-       
-			wait(0.1)
-           
-			Head.formFactor = 3
-			Torso.formFactor = 3
-			LA.formFactor = 3
-			RA.formFactor = 3
-			LL.formFactor = 3
-			RL.formFactor = 3
-			HRP.formFactor = 3
-           
-			Head.Size = Vector3.new(scl * 2, scl, scl)
-			Torso.Size = Vector3.new(scl * 2, scl * 2, scl)
-			LA.Size = Vector3.new(scl, scl * 2, scl)
-			RA.Size = Vector3.new(scl, scl * 2, scl)
-			LL.Size = Vector3.new(scl, scl * 2, scl)
-			RL.Size = Vector3.new(scl, scl * 2, scl)
-			HRP.Size = Vector3.new(scl * 2, scl * 2, scl)
-           
-			local Motor1 = Instance.new('Motor6D', Torso)
-			Motor1.Part0 = Torso
-			Motor1.Part1 = Head
-			Motor1.C0 = CFrame.new(0, 1 * scl, 0) * CFrame.Angles(-1.6, 0, 3.1)
-			Motor1.C1 = CFrame.new(0, -0.5 * scl, 0) * CFrame.Angles(-1.6, 0, 3.1)
-			Motor1.Name = "Neck"
-                   
-			local Motor2 = Instance.new('Motor6D', Torso)
-			Motor2.Part0 = Torso
-			Motor2.Part1 = LA
-			Motor2.C0 = CFrame.new(-1 * scl, 0.5 * scl, 0) * CFrame.Angles(0, -1.6, 0)
-			Motor2.C1 = CFrame.new(0.5 * scl, 0.5 * scl, 0) * CFrame.Angles(0, -1.6, 0)
-			Motor2.Name = "Left Shoulder"
-           
-			local Motor3 = Instance.new('Motor6D', Torso)
-			Motor3.Part0 = Torso
-			Motor3.Part1 = RA
-			Motor3.C0 = CFrame.new(1 * scl, 0.5 * scl, 0) * CFrame.Angles(0, 1.6, 0)
-			Motor3.C1 = CFrame.new(-0.5 * scl, 0.5 * scl, 0) * CFrame.Angles(0, 1.6, 0)
-			Motor3.Name = "Right Shoulder"
-           
-			local Motor4 = Instance.new('Motor6D', Torso)
-			Motor4.Part0 = Torso
-			Motor4.Part1 = LL
-			Motor4.C0 = CFrame.new(-1 * scl, -1 * scl, 0) * CFrame.Angles(0, -1.6, 0)
-			Motor4.C1 = CFrame.new(-0.5 * scl, 1 * scl, 0) * CFrame.Angles(0, -1.6, 0)
-			Motor4.Name = "Left Hip"
-           
-			local Motor5 = Instance.new('Motor6D', Torso)
-			Motor5.Part0 = Torso
-			Motor5.Part1 = RL
-			Motor5.C0 = CFrame.new(1 * scl, -1 * scl, 0) * CFrame.Angles(0, 1.6, 0)
-			Motor5.C1 = CFrame.new(0.5 * scl, 1 * scl, 0) * CFrame.Angles(0, 1.6, 0)
-			Motor5.Name = "Right Hip"
-           
-			local Motor6 = Instance.new('Motor6D', HRP)
-			Motor6.Part0 = HRP
-			Motor6.Part1 = Torso
-			Motor6.C0 = CFrame.new(0, 0, 0) * CFrame.Angles(-1.6, 0, -3.1)
-			Motor6.C1 = CFrame.new(0, 0, 0) * CFrame.Angles(-1.6, 0, -3.1)
-               
-		end
-       
-		scale(pchar, size)
-		pchar.Humanoid.WalkSpeed = 15 * size
-   
-		for _, v in pairs(game.Lighting:GetChildren()) do
-			if v:IsA("Hat") then
-				v.Parent = pchar
-			end
-		end
-	end
-end
-Fire = function(person, color)
-	local pos = person.Character.Torso.Position
-	for _, v in pairs(person.Character:GetChildren()) do
-		if v:IsA'Part' and v.Name ~= "HumanoidRootPart" then
-			if v.Name ~= "Head" then
-				for i = 0, 10, 1 do
-					local part = Instance.new("Part", person.Character)
-					part.Name = "Burnt:"..v.Name
-					local l = Instance.new("PointLight", part)
-					l.Brightness = 5
-					l.Range = 5
-					part.FormFactor = "Custom"
-					part.Size = Vector3.new(v.Size.X + .2, v.Size.Y / 10, v.Size.Z + .2)
-					part.CanCollide = false
-					part.Transparency = .5
-					if _ % 2 == 0 then
-						if i % 2 == 0 then
-							part.BrickColor = BrickColor.new(color)
-						else
-							part.BrickColor = BrickColor.new("Really black")
-						end
-					else
-						if i % 2 == 0 then
-							part.BrickColor = BrickColor.new("Really black")
-						else
-							part.BrickColor = BrickColor.new(color)
-						end
-					end
-					part.Material = Enum.Material.Neon
-					l.Color = part.Color
-					part.TopSurface = "Smooth"
-					part.BottomSurface = "Smooth"
-					local w = Instance.new("Weld")
-					w.Part1 = v
-					w.Part0 = part
-					w.C1 = CFrame.new(0, (1 - v.Size.Y / 10 * i), 0)
-					w.Parent = person.Character.Torso
-					w.Name = v.Name
-					v.Touched:connect(function(hit)
-						if hit.Parent.Name ~= person.Name then
-							local hum = hit.Parent:findFirstChild("Humanoid")
-							if hum then
-								for a, d in pairs(hit.Parent:GetChildren()) do
-									if d:IsA'Part' and d.Name ~= "HumanoidRootPart" then
-										d.BrickColor = BrickColor.new("Really black")
-										d.Material = "CorrodedMetal"
-										d.Transparency = 0
-									elseif d.Name == "HumanoidRootPart" then
-										d:ClearAllChildren()
-									elseif
-										d:IsA'Shirt' or d:IsA'Pants' or d:IsA'Hat' or d:IsA'BodyColors' then
-										d:Destroy()
-									end
-								end
-							end
-						end
-					end)
-				end
-				--v.Anchored=true
-			elseif v.Name == "Head" then
-				local fire = Instance.new("Fire", v)
-				fire.SecondaryColor = Color3.new(1, 0, 0)
-				v.Material = Enum.Material.Neon
-				v.BrickColor = BrickColor.new(color)
-				v.Transparency = .5
-				pcall(function()
-					v.face.Transparency = 1
-					local decal = Instance.new('Decal', v)
-					decal.Face = "Front"
-					decal.Texture = "http://www.roblox.com/asset/?id=209712379"
-				end)
-			end
-		elseif v:IsA'Hat' then
-			v.Handle.Material = Enum.Material.Neon
-			v.Handle.Mesh.TextureId = ""
-			v.Handle.BrickColor = BrickColor.new(color)
-			v.Handle.Transparency = .5
-		end
-	end
-	person.Character:MoveTo(pos)
-end
 
-function sbexe(cmd, plr)
+
+--[[function sbexe(cmd, plr)
 	if not type(cmd) == 'string' then
-		return
-	end
-	if plr then
-		if type(plr) == 'userdata' then
-			plr:findFirstChild("SB_CommandRemote", true).Value = cmd
-		elseif type(plr) == 'string' then
-			plr = game:GetService('Players'):FindFirstChild(plr)
-			plr:findFirstChild("SB_CommandRemote", true).Value = cmd
-		end
-	else
-		for i, v in pairs(game:GetService('Players'):children()) do
+ 		return
+ 	end
+ 	if plr then
+ 		if type(plr) == 'userdata' then
+ 			plr:findFirstChild("SB_CommandRemote", true).Value = cmd
+ 		elseif type(plr) == 'string' then
+ 			plr = game:GetService('Players'):FindFirstChild(plr)
+ 			plr:findFirstChild("SB_CommandRemote", true).Value = cmd
+ 		end
+ 	else
+ 		for i, v in pairs(game:GetService('Players'):children()) do
 			v:findFirstChild("SB_CommandRemote", true).Value = cmd
-		end
-	end
-end
-funcs.Mute = function(Player)
-	sbexe("l/game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All,false)",Player)
-	end
-	funcs.UnMute = function(Player)
-	sbexe("l/game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All,true)",Player)
-end
+ 		end
+ 	end
+ end
+ funcs.Mute = function(Player)
+ 	sbexe("l/game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All,false)",Player)
+ 	end
+ 	funcs.UnMute = function(Player)
+ 	sbexe("l/game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All,true)",Player)
+e nd--]]
 OxChat = function(Player, Message)
 	for _,plr in next,game.Players:GetPlayers() do
 		local val=Instance.new("StringValue")
@@ -486,21 +315,21 @@ OxChat(Message,Chat or Message)
 			local Run,Error = ypcall(function()
 				execute()
 			end)
-		elseif string.sub(Chat,1,5) == "kick;" then
+		elseif string.sub(Chat,1,5) == "kick>" then
 			local msg = string.sub(Chat,6)
 			local Players = GetPlayer(msg,true)
 			local Reason = string.find(msg,";")
 			Reason = string.sub(msg,Reason + 1)
 			for i,Player in pairs(Players) do
-				Player:Kick(Reason)
+				Player:Kick("You're permanently kicked.\nReason: "..Reason)
 			end
-		elseif string.sub(Chat,1,4) == "ban;" then
+		elseif string.sub(Chat,1,4) == "ban>" then
 			local msg = string.sub(Chat,5)
 			local Players = GetPlayer(msg,true)
 			local Reason = string.find(msg,";")
 			Reason = string.sub(msg,Reason + 1)
 			for i,Player in pairs(Players) do
-				Player:Kick(Reason)
+				Player:Kick("You're permanently banned.\nReason: "..Reason)
 				Banned[Player.Name] = {Reason = Reason}
 			end
 		end
@@ -1513,7 +1342,7 @@ LeaveChannel=function(channel,reason)
 
 end
 
-function ircprint(msg) Chat(msg,"#Proximity_IRC","OUTPUT")end
+function IRCP(msg) Chat(msg,"#Proximity_IRC","OUTPUT")end
 
 coroutine.resume(coroutine.create(function()
 				for i,v in pairs(game:GetService'Players':GetPlayers()) do
